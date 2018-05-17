27ee0f243dade05082249eed35abca5ec6cb2bb6
gonzalo <greco_gonzalo@hotmail.com>
2018-05-17 10:25:13 -0300

    nomeclatura punteros

--- nomenclatura/main.c
+++ nomenclatura/main.c
@@ -3,6 +3,26 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    // Punteros
+    int *punteroNumero;
+    int numero;
+    int numero2;
+    numero = 66;
+    numero2 = &numero;
+
+    punteroNumero = &numero;
+    //*punteroNumero = &numero;
+    //*punteroNumero = 55;
+    printf("\na- %d\n", numero2);
+    numero2 = *punteroNumero;
+    printf("\nb- %d\n", numero2);
+
+    printf(" %d ", numero);
+    printf(" %d ", numero2);
+    printf(" %d ", &numero);
+    printf(" %p ", punteroNumero);
+    printf(" %p ", &punteroNumero);
+    printf(" %d ", *punteroNumero);
+    //printf("%d", *punteroNumero);
     return 0;
 }
